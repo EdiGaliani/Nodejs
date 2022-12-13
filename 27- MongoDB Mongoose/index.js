be4e9f552889a5ae32c533ab9721aa9bda57ef8e
galiano <edigaliano@gmail.com>
2022-12-13 17:18:36 -0300

    Buscando dados

--- 27- MongoDB Mongoose/index.js
+++ 27- MongoDB Mongoose/index.js
@@ -8,14 +8,36 @@ mongoose.connect('mongodb://127.0.0.1:27017/aprendendoMongo').then(db => {
 
 const Article = mongoose.model("Article", articleModel);
 
-const article = new Article({
-    title: "Meu 1º artigo",
-    author: "Edi",
-    body: "Bem vindos ao meu primeiro artigo"
-});
-
-article.save().then((db) => {
-    console.log(db);
-}).catch((err) => {
+Article.find({'_id':'6398d2f29a03d688fd050160'}).then(articles => {
+    console.log(articles);
+}).catch(err => {
     console.log(err);
 });
+
+// //Buscando Todos Registros
+// Article.find({}).then(articles => {
+//     console.log(articles);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+
+
+
+// //Cadastro
+// const article = new Article({
+//     title: "Meu 1º artigo",
+//     author: "Edi",
+//     body: "Bem vindos ao meu primeiro artigo",
+//     special: true,
+//     resume: {
+//         content: "Bla Bla Bla",
+//         author: "Galiani"
+//     }
+// });
+
+// article.save().then((db) => {
+//     console.log(db);
+// }).catch((err) => {
+//     console.log(err);
+// });
