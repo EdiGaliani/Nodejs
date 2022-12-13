e7d8ec857f2966bb1961cae22dca989d09a7478e
galiano <edigaliano@gmail.com>
2022-12-13 16:26:27 -0300

    salvando dados

--- 27- MongoDB Mongoose/index.js
+++ 27- MongoDB Mongoose/index.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
+const articleModel = require('./article');
 
 mongoose.set('strictQuery', true)
 mongoose.connect('mongodb://127.0.0.1:27017/aprendendoMongo').then(db => {
+    //console.log(db);
+});
+
+const Article = mongoose.model("Article", articleModel);
+
+const article = new Article({
+    title: "Meu 1º artigo",
+    author: "Edi",
+    body: "Bem vindos ao meu primeiro artigo"
+});
+
+article.save().then((db) => {
     console.log(db);
+}).catch((err) => {
+    console.log(err);
 });
