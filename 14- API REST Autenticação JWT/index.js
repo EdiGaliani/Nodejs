69f6c16f641c5b1cee08bccfd74bdf5452248d17
galiano <edigaliano@gmail.com>
2022-12-27 09:55:13 -0300

    Autenticando API

--- 14- API REST Autenticação JWT/index.js
+++ 14- API REST Autenticação JWT/index.js
@@ -9,6 +9,27 @@ app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+function auth(req, res , next) {
+    const authToken = req.headers['authorization'];
+    if(authToken != undefined) {
+        const bearer = authToken.split(' ');
+        let token = bearer[1];
+        jwt.verify(token, JWTSecret, (err, data) => {
+            if(err) {
+                res.status(401);
+                res.json({err:"Token Inválido!"});
+            }else {
+                req.token = token;
+                req.loggedUser = {id: data.id, email: data.email};
+                next();
+            }
+        })
+    }else {
+        res.status(401);
+        res.json({err:"Token Inválido!"});
+    }
+}
+
 const DB = {
     games: [
         {
@@ -47,7 +68,7 @@ const DB = {
     ]
 }
 
-app.get("/games", (req, res) => {
+app.get("/games", auth, (req, res) => {
     res.statusCode = 200;
     res.json(DB.games);
 });
